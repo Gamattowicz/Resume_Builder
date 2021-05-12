d76f78158238b32f1f4e28b5f2f830b9434a5808
Gamattowi <p.romanczuk31@gmail.com>
2021-05-12 22:48:58 +0200

    add form to create new resume

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, resume
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('resume', resume)
 ]
--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .forms import CreateResume
+from .models import Resume
 
 
 def home(response):
     return render(response, 'api/home.html', {})
+
+
+def resume(response):
+    if response.method == 'POST':
+        form = CreateResume(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = Resume(name=n)
+            t.save()
+    else:
+        form = CreateResume()
+    return render(response, 'api/resume.html', {"form":form})
