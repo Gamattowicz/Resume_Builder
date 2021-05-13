0771babd2a74cb38e62f28080ad9b65ee1068ed6
Gamattowi <p.romanczuk31@gmail.com>
2021-05-13 12:52:12 +0200

    add crispy forms

--- resume_builder/resume_builder/settings.py
+++ resume_builder/resume_builder/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'api.apps.ApiConfig',
     'user.apps.UserConfig',
     'rest_framework'
@@ -129,3 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
