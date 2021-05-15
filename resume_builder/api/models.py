e300b2287519b4dcd0703deaab87e5efaa534e56
Gamattowi <p.romanczuk31@gmail.com>
2021-05-15 14:56:24 +0200

    add new fields to Resume model

--- resume_builder/api/models.py
+++ resume_builder/api/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import User
 
 class Resume(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume', null=True)
-    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.DecimalField(max_digits=9, decimal_places=9)
+    lin = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.first_name, self.last_name
