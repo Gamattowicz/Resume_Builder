22160c77be3b8fa1a54ef71142977fe39b41e847
Gamattowi <p.romanczuk31@gmail.com>
2021-05-15 17:59:23 +0200

    add new forms fields

--- resume_builder/api/forms.py
+++ resume_builder/api/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 
 
 class CreateResume(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
+    first_name = forms.CharField(label="First name", max_length=200)
+    last_name = forms.CharField(label="Last name", max_length=200)
+    email = forms.EmailField(label="Email", max_length=200)
+    phone = PhoneNumberField(label="Phone number")
+    lin = forms.URLField(max_length=200)
--- resume_builder/api/models.py
+++ resume_builder/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Resume(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    phone = models.DecimalField(max_digits=9, decimal_places=9)
+    phone = PhoneNumberField()
     lin = models.URLField(max_length=200)
 
     def __str__(self):
