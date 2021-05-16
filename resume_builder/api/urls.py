6d53b779e66e76d1785b02b72f82b8d5c8fe47f4
Gamattowi <p.romanczuk31@gmail.com>
2021-05-16 16:44:20 +0200

    add creating PDF file

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('resume/', views.resume, name='resume'),
     path('view/', views.view, name='view'),
-    path('<int:resume_id>/', views.detail, name='detail')
+    path('<int:resume_id>/', views.detail, name='detail'),
+    path('pdf/', views.render_pdf_view, name='pdf'),
 ]
--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import CreateResume
 from .models import Resume
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 
 def home(request):
@@ -33,6 +36,25 @@ def view(request):
 def detail(request, resume_id):
     resume = get_object_or_404(Resume, pk=resume_id)
     return render(request, 'api/detail.html', {'resume': resume})
+
+
+def render_pdf_view(request):
+    template_path = 'api/testing.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 
 
