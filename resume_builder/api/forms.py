005f2e29c475c87f66ede94081daf9e5503203b6
Gamattowi <p.romanczuk31@gmail.com>
2021-05-17 19:45:19 +0200

    add first tests

--- resume_builder/api/forms.py
+++ resume_builder/api/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 
 
+
 class CreateResume(forms.Form):
     first_name = forms.CharField(label="First name", max_length=200)
     last_name = forms.CharField(label="Last name", max_length=200)
--- resume_builder/api/tests.py
+++ resume_builder/api/tests.py
@@ -1,3 +1,43 @@
+from http import HTTPStatus
+from datetime import datetime
 from django.test import TestCase
+from .forms import CreateResume
 
 # Create your tests here.
+
+
+class TestForms(TestCase):
+
+    def test_resume_form_valid_data(self):
+        form = CreateResume(data={
+            'first_name': 'Jim',
+            'last_name': 'Jarmusch',
+            'email': 'jim@gmail.com',
+            'phone': '+12123456789',
+            'lin': 'google.com',
+            'hobby':  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'skills': '''Cras tempus vitae erat vel faucibus. Integer ornare eros id nulla vehicula auctor. 
+                          Aenean hendrerit libero ut diam varius vestibulum.''',
+            'school': 'University',
+            'school_city': 'Tom',
+            'degree': 'Bachelor',
+            'field_study': 'IT',
+            'start_date': datetime(2015, 3, 21),
+            'end_date': datetime(2020, 6, 3),
+            'description': '''Morbi vehicula in mauris faucibus fermentum. Ut molestie magna non malesuada luctus. 
+                               Vestibulum facilisis accumsan tristique. Cras at arcu metus. ''',
+            'company': 'Tonya',
+            'exp_city': 'Warsaw',
+            'position': 'Junior',
+            'start_date_exp':  datetime(2020, 7, 1),
+            'end_date_exp': datetime(2021, 6, 6),
+            'description_exp': '''Duis quis felis tristique, tempus libero sed, elementum turpis. Etiam pellentesque 
+                                   tellus turpis, dapibus volutpat mi sollicitudin sed. '''
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_resume_form_no_data(self):
+        form = CreateResume(data={})
+
+        self.assertFalse(form.is_valid())
