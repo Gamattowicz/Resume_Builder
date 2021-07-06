ca56b534e35f85884154e0063ba6a779d9379259
Gamattowi <p.romanczuk31@gmail.com>
2021-07-06 11:52:23 +0200

    move Resume model to separate app

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume', null=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = PhoneNumberField()
+    lin = models.URLField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{str(self.first_name)} {str(self.last_name)}'
