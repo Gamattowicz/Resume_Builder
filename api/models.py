7549f5288ea4d1626a68c9d4ad19f08c49372a90
Gamattowi <p.romanczuk31@gmail.com>
2021-07-06 12:29:04 +0200

    move Hobby model to separate app

--- api/models.py
+++ api/models.py
@@ -26,11 +26,3 @@ class Experience(models.Model):
 
     def __str__(self):
         return f'{str(self.company)}'
-
-
-class Hobby(models.Model):
-    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, related_name='hobby', null=True)
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return f'{str(self.name)}'
--- hobby/models.py
+++ hobby/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from resumes.models import Resume
 
-# Create your models here.
+
+class Hobby(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, related_name='hobby', null=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{str(self.name)}'
