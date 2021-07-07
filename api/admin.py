e73d64a6bfd61a31c7926103051e6188dd97ddff
Gamattowi <p.romanczuk31@gmail.com>
2021-07-07 14:12:06 +0200

    move Experience model to separate app

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import Experience
-
-# Register your models here.
-
-admin.site.register(Experience)
--- experiences/models.py
+++ experiences/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from resumes.models import Resume
 
-# Create your models here.
+
+class Experience(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, related_name='experience', null=True)
+    company = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f'{str(self.company)}'
