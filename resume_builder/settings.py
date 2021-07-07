69f807aa51168aa9b9259a0ed5dd76dcc6c23f38
Gamattowi <p.romanczuk31@gmail.com>
2021-07-07 20:50:34 +0200

    move base.html to main templates directory

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,7 +70,8 @@ ROOT_URLCONF = 'resume_builder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'.\api\templates\api'],
+        'DIRS': [r'.\api\templates\api',
+                 os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
