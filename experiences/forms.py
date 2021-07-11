5700d3d571b35ced9dccf33d66b3baae7f8bb232
Gamattowi <p.romanczuk31@gmail.com>
2021-07-11 19:06:06 +0200

    create ExperienceForms

--- experiences/forms.py
+++ experiences/forms.py
@@ -1,4 +1,12 @@
+from django import forms
 from django.forms import modelformset_factory
 from .models import Experience
 
-ExperienceFormSet = modelformset_factory(Experience, fields='__all__', extra=1)
+
+class ExperienceForms(forms.ModelForm):
+    class Meta:
+        model = Experience
+        fields = ['company', 'city', 'position', 'description', 'start_date', 'end_date']
+
+
+ExperienceFormSet = modelformset_factory(Experience, fields=('company', 'city', 'position', 'description', 'start_date', 'end_date'), extra=1)
