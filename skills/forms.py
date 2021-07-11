a423e686a4b28a862f01c6c31960f2e5d756a364
Gamattowi <p.romanczuk31@gmail.com>
2021-07-11 19:18:20 +0200

    create SkillForms

--- skills/forms.py
+++ skills/forms.py
@@ -2,4 +2,11 @@ from django import forms
 from django.forms import modelformset_factory
 from .models import Skill
 
-SkillFormSet = modelformset_factory(Skill, fields='__all__', extra=1)
+
+class SkillForms(forms.ModelForm):
+    class Meta:
+        model = Skill
+        fields = ['name', 'level']
+
+
+SkillFormSet = modelformset_factory(Skill, fields=('name', 'level'), extra=1)
