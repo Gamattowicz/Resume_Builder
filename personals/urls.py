3fd91758be04f257224fefc745b1cc100fe395d0
Gamattowi <p.romanczuk31@gmail.com>
2021-07-11 17:22:46 +0200

    create PersonalUpdateView

--- personals/urls.py
+++ personals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PersonalCreateView
+from .views import PersonalCreateView, PersonalUpdateView
 
 app_name = 'personals'
 
 urlpatterns = [
     path('create/', PersonalCreateView.as_view(), name='create_personal'),
+    path('<int:pk>/update', PersonalUpdateView.as_view(), name='update_personal'),
 ]
--- personals/views.py
+++ personals/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Personal
 from .forms import PersonalForms
@@ -13,3 +13,10 @@ class PersonalCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(PersonalCreateView, self).form_valid(form)
+
+
+class PersonalUpdateView(LoginRequiredMixin, UpdateView):
+    model = Personal
+    form_class = PersonalForms
+    success_url = reverse_lazy('schools:add_school')
+    template_name = 'personals/personal_update.html'
