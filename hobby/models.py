01372df9d02ee089d0ce4d71a1f08540a79efeae
Gamattowi <p.romanczuk31@gmail.com>
2021-07-12 20:26:04 +0200

    add correct plural name to hobby model

--- hobby/models.py
+++ hobby/models.py
@@ -6,5 +6,9 @@ class Hobby(models.Model):
     resume = models.ForeignKey(Personal, on_delete=models.CASCADE, related_name='hobby', null=True)
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = "Hobby"
+        verbose_name_plural = "Hobbies"
+
     def __str__(self):
         return f'{str(self.name)}'
