ec001de3f7ca70b01fe26ec9fc8213911ec5fdb1
Gamattowi <p.romanczuk31@gmail.com>
2021-07-12 19:42:56 +0200

    create HobbyForms

--- hobby/forms.py
+++ hobby/forms.py
@@ -2,4 +2,11 @@ from django import forms
 from django.forms import modelformset_factory
 from .models import Hobby
 
+
+class HobbyForms(forms.ModelForm):
+    class Meta:
+        model = Hobby
+        fields = ['name']
+
+
 HobbyFormSet = modelformset_factory(Hobby, fields=('name',), extra=1)
