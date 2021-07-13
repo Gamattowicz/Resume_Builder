9d17ef15cda3dc764d96ae7c3cb4b5ee08e3c48a
Gamattowi <p.romanczuk31@gmail.com>
2021-07-13 13:35:09 +0200

    add placeholders for ExperienceForms

--- experiences/forms.py
+++ experiences/forms.py
@@ -8,5 +8,31 @@ class ExperienceForms(forms.ModelForm):
         model = Experience
         fields = ['company', 'city', 'position', 'description', 'start_date', 'end_date']
 
+    company = forms.CharField(
+        label='Company Name',
+        widget=forms.TextInput(attrs={'placeholder': 'Google'})
+    )
+    city = forms.CharField(
+        label='City',
+        widget=forms.TextInput(attrs={'placeholder': 'Mountain View'})
+    )
+    position = forms.CharField(
+        label='Position',
+        widget=forms.TextInput(attrs={'placeholder': 'Junior Backend'})
+    )
+    description = forms.CharField(
+        label='Job description',
+        widget=forms.Textarea(attrs={'placeholder': 'Describe your experience'})
+    )
+    start_date = forms.DateField(
+        label='Start date',
+        widget=forms.DateInput(attrs={'placeholder': '2015-10-01'})
+    )
+    end_date = forms.DateField(
+        label='End date',
+        widget=forms.DateInput(attrs={'placeholder': '2018-07-15`'})
+    )
 
-ExperienceFormSet = modelformset_factory(Experience, fields=('company', 'city', 'position', 'description', 'start_date', 'end_date'), extra=1)
+
+ExperienceFormSet = modelformset_factory(Experience, form=ExperienceForms, fields=('company', 'city', 'position',
+                                                                                   'description', 'start_date', 'end_date'), extra=1)
