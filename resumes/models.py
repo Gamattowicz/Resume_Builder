cf2f7a0d3058a1b47404860faa467ee5f2f28542
Gamattowi <p.romanczuk31@gmail.com>
2021-07-13 22:43:27 +0200

    fix Resume model

--- resumes/models.py
+++ resumes/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Resume(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume', null=True)
     template = models.CharField(max_length=200, null=True, blank=True)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{str(self.template)}'
