b538f8b2a76ccdf1074aac132d1bc04bcd71f2e4
Gamattowi <p.romanczuk31@gmail.com>
2021-07-13 20:40:43 +0200

    create Resume model

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume', null=True)
+    template = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f'{str(self.template)}'
