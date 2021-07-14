5db3e2002c33c921ad0118ff15f926d3ac3b62fb
Gamattowi <p.romanczuk31@gmail.com>
2021-07-14 20:52:05 +0200

    create ResumeListView

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ResumeCreateView
+from .views import ResumeCreateView, ResumeListView
 
 app_name = 'resumes'
 
 urlpatterns = [
     path('create/', ResumeCreateView.as_view(), name='create_resume'),
+    path('', ResumeListView.as_view(), name='resumes'),
 ]
--- resumes/views.py
+++ resumes/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Resume
 from .forms import ResumeForms
@@ -11,3 +12,8 @@ class ResumeCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(ResumeCreateView, self).form_valid(form)
+
+
+class ResumeListView(LoginRequiredMixin, ListView):
+    model = Resume
+    context_object_name = 'resumes'
