896f35d6ac1bcbfe22e97ff7b24148c0a938f916
Gamattowi <p.romanczuk31@gmail.com>
2021-07-16 08:05:23 +0200

    add ImageField to Personal model

--- personals/models.py
+++ personals/models.py
@@ -8,6 +8,7 @@ class Personal(models.Model):
     resume = models.OneToOneField(Resume, on_delete=models.CASCADE, related_name='personal')
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    photo = models.ImageField(null=True, blank=True, upload_to='images', default='/user_default.png')
     email = models.EmailField(max_length=200)
     phone = PhoneNumberField(blank=True)
     lin = models.URLField(max_length=200)
