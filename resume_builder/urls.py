40d72ca2ab73fd57e25d7254da067d88019c7033
Gamattowi <p.romanczuk31@gmail.com>
2021-07-16 08:04:59 +0200

    add url to MEDIA and STATIC ROOT

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -29,3 +31,6 @@ urlpatterns = [
     path('', include('api.urls', namespace='api')),
     path('', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
