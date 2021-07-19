65af4c217e80880fd4b45ad5c7595c318ada2b59
Gamattowi <p.romanczuk31@gmail.com>
2021-07-19 11:58:50 +0200

    add PasswordResetView

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -1,22 +1,8 @@
-"""resume_builder URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -30,6 +16,8 @@ urlpatterns = [
     path('resumes/', include('resumes.urls', namespace='resumes')),
     path('', include('api.urls', namespace='api')),
     path('', include('django.contrib.auth.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
