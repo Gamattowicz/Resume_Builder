b17591a2398f37394e74dae326535293e00fd4d4
Gamattowi <p.romanczuk31@gmail.com>
2021-07-19 11:51:20 +0200

    add generated an app password to email account

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -143,7 +143,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+EMAIL_HOST_PASSWORD = 'tciepblumplyhmmq'
 
 
 # Static files (CSS, JavaScript, Images)
