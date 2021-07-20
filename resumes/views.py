e8e95b71aa0aca51bdd75ab5fbcb8033ffe5bfc5
Gamattowi <p.romanczuk31@gmail.com>
2021-07-20 19:52:42 +0200

    create ResumeDeleteView

--- resumes/views.py
+++ resumes/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Resume
 from .forms import ResumeForms
 
@@ -28,3 +30,10 @@ class ResumeDetailView(LoginRequiredMixin, DetailView):
     model = Resume
     context_object_name = 'resume'
     template_name = 'resume_2.html'
+
+
+class ResumeDeleteView(LoginRequiredMixin, DeleteView):
+    model = Resume
+    context_object_name = 'resume'
+    success_url = reverse_lazy('resumes:resumes')
+    template_name = 'resume_delete.html'
