ea984494100f90c0ea25af88b60a1a09361597c4
Gamattowi <p.romanczuk31@gmail.com>
2021-07-20 19:53:41 +0200

    create resume_delete.html

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ResumeCreateView, ResumeListView, ResumeDetailView
+from .views import ResumeCreateView, ResumeListView, ResumeDetailView, ResumeDeleteView
 
 app_name = 'resumes'
 
 urlpatterns = [
+    path('', ResumeListView.as_view(), name='resumes'),
     path('create/', ResumeCreateView.as_view(), name='create_resume'),
     path('<int:pk>/', ResumeDetailView.as_view(), name='resume'),
-    path('', ResumeListView.as_view(), name='resumes'),
+    path('<int:pk>/delete', ResumeDeleteView.as_view(), name='delete_resume'),
 ]
