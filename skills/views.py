cea7c5a4c6c466b75ffa7fed09255e38ce24026e
Gamattowi <p.romanczuk31@gmail.com>
2021-07-21 20:41:13 +0200

    fix SkillUpdateView and add success_message

--- skills/views.py
+++ skills/views.py
@@ -4,12 +4,14 @@ from django.views.generic.edit import UpdateView
 from django.views.generic import ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Skill
-from .forms import SkillFormSet, SkillForms
+from .forms import SkillFormSet
 from resumes.models import Resume
+from django.contrib import messages
 
 
 class SkillCreateView(LoginRequiredMixin, TemplateView):
     template_name = 'skill_form.html'
+    success_message = 'Skill was created successfully'
 
     def get(self, *args, **kwargs):
         formset = SkillFormSet(queryset=Skill.objects.none())
@@ -29,10 +31,23 @@ class SkillCreateView(LoginRequiredMixin, TemplateView):
 
 
 class SkillUpdateView(LoginRequiredMixin, UpdateView):
-    model = Skill
-    form_class = SkillForms
-    success_url = reverse_lazy('hobby:create_hobby')
-    template_name = 'skills/skill_update.html'
+    model = 'Skill'
+    template_name = 'skill_update.html'
+    success_url = reverse_lazy('resumes:resumes',)
+    success_message = 'Skill was updated successfully'
+
+    def get(self, *args, **kwargs):
+        formset = SkillFormSet(queryset=Skill.objects.filter(resume_id=self.kwargs['pk']))
+        return self.render_to_response({'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = SkillFormSet(data=self.request.POST)
+
+        if formset.is_valid():
+            formset.save()
+            messages.success(self.request, self.success_message)
+            return redirect(reverse_lazy('resumes:resumes'))
+        return self.render_to_response({'formset': formset})
 
 
 class SkillListView(LoginRequiredMixin, ListView):
