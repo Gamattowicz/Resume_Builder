2a3b6519671029a03770e5a02454ea2ca45ae286
Gamattowi <p.romanczuk31@gmail.com>
2021-07-28 13:54:07 +0200

    style password_reset_confirm.html

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'),
          name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
          name="password_reset_confirm"),
 
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
