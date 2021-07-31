ebecf6e4a87eb5cd5ecd0e9dc525b6b518526a5d
Gamattowi <p.romanczuk31@gmail.com>
2021-07-31 19:19:34 +0200

    fix saving experiences in ExperienceCreateView

--- experiences/views.py
+++ experiences/views.py
@@ -13,12 +13,12 @@ class ExperienceCreateView(LoginRequiredMixin, CreateView):
     template_name = 'experience_form.html'
     success_message = "Experience was created successfully"
 
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         form = ExperienceForms()
         formset = ExperienceDescriptionFormSet(queryset=ExperienceDescription.objects.none())
-        return self.render_to_response({'form': form, 'formsets': formset})
+        return self.render_to_response({'form': form, 'formset': formset})
 
-    def post(self, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         form = ExperienceForms(data=self.request.POST)
         formset = ExperienceDescriptionFormSet(data=self.request.POST)
         if form.is_valid() and formset.is_valid():
@@ -29,12 +29,15 @@ class ExperienceCreateView(LoginRequiredMixin, CreateView):
             description_instances = formset.save(commit=False)
             for description_instance in description_instances:
                 resume_id = self.kwargs['pk']
-                description_instance.experience = Experience.objects.get(resume_id=resume_id)
+                description_instance.experience = Experience.objects.filter(resume_id=resume_id).last()
                 description_instance.save()
             form.save()
             formset.save()
-            return redirect(reverse_lazy('skills:create_skill', kwargs={'pk': self.kwargs['pk']}))
-        return self.render_to_response({'form': form, 'formsets': formset})
+            if 'create_exp' in self.request.POST:
+                return redirect(reverse_lazy('experiences:create_experience', kwargs={'pk': self.kwargs['pk']}))
+            else:
+                return redirect(reverse_lazy('skills:create_skill', kwargs={'pk': self.kwargs['pk']}))
+        return self.render_to_response({'form': form, 'formset': formset})
 
 
 class ExperienceUpdateView(LoginRequiredMixin, UpdateView):
