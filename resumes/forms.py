66f9936e956929085aa31063e7fabccbfff9f4f6
Gamattowi <p.romanczuk31@gmail.com>
2021-08-03 16:24:54 +0200

    center thumbnails in create resume

--- resumes/forms.py
+++ resumes/forms.py
@@ -5,7 +5,7 @@ from .models import Resume
 
 TEMPLATES_CHOICES = (
     ("1", "One"),
-    ("2", mark_safe('<img src="/images/resume_2_thumbnail.PNG" class="img-fluid d-block w-50" alt="Responsive image">')),
+    ("2", mark_safe('<img src="/images/resume_2_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
     ("3", "Three"),
 )
 
