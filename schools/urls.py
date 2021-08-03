8c80693fc9313c1d1705c8c887aa7d4e8e0b4b3c
Gamattowi <p.romanczuk31@gmail.com>
2021-08-03 16:27:26 +0200

    delete SchoolListView

--- schools/urls.py
+++ schools/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SchoolCreateView, SchoolUpdateView, SchoolListView
+
+from .views import SchoolCreateView, SchoolUpdateView
 
 app_name = 'schools'
 
 urlpatterns = [
     path('<int:pk>/create/', SchoolCreateView.as_view(), name='create_school'),
     path('<int:pk>/update', SchoolUpdateView.as_view(), name='update_school'),
-    path('', SchoolListView.as_view(), name='school_list'),
 ]
