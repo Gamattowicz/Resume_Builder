70896df1b636b974662f77432c8c41ef5c6d673b
Gamattowi <p.romanczuk31@gmail.com>
2021-08-03 14:08:43 +0200

    add displaying thumbnails in create resume

--- resumes/forms.py
+++ resumes/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from .models import Resume
+from django.utils.safestring import mark_safe
 
+from .models import Resume
 
 TEMPLATES_CHOICES = (
     ("1", "One"),
-    ("2", "Two"),
+    ("2", mark_safe('<img src="/images/resume_2_thumbnail.PNG" class="img-fluid d-block w-50" alt="Responsive image">')),
     ("3", "Three"),
 )
 
@@ -16,5 +17,6 @@ class ResumeForms(forms.ModelForm):
 
     template = forms.ChoiceField(
         label='Choose a template',
-        choices=TEMPLATES_CHOICES
+        choices=TEMPLATES_CHOICES,
+        widget=forms.widgets.RadioSelect
     )
