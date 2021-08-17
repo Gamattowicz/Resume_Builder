316f3566d54314299a13abd6f436d79b55110266
Gamattowi <p.romanczuk31@gmail.com>
2021-08-17 11:13:12 +0200

    add thumbnail to resume_1

--- resumes/forms.py
+++ resumes/forms.py
@@ -4,7 +4,7 @@ from django.utils.safestring import mark_safe
 from .models import Resume
 
 TEMPLATES_CHOICES = (
-    ("1", "One"),
+    ("1", mark_safe('<img src="/images/resume_1_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
     ("2", mark_safe('<img src="/images/resume_2_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
     ("3", "Three"),
 )
