995379259809f6640552627f5ccef23ff7ca9c2c
Gamattowi <p.romanczuk31@gmail.com>
2021-08-17 13:36:49 +0200

    add thumbnail to resume_3

--- resumes/forms.py
+++ resumes/forms.py
@@ -6,7 +6,7 @@ from .models import Resume
 TEMPLATES_CHOICES = (
     ("1", mark_safe('<img src="/images/resume_1_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
     ("2", mark_safe('<img src="/images/resume_2_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
-    ("3", "Three"),
+    ("3", mark_safe('<img src="/images/resume_3_thumbnail.PNG" class="img-fluid d-block w-50 mx-auto" alt="Responsive image">')),
 )
 
 
